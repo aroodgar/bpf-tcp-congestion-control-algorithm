edda858eb20650b983109ac7bbda667c4cc56eea
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-06-02 19:16:31 +0330

    feat: moved files to new src directory

--- src/compile_and_load.sh
+++ src/compile_and_load.sh
@@ -4,3 +4,4 @@
 clang-13 -target bpf -I/usr/include/$(uname -m)-linux-gnu -g -O2 -o bpf_cubic.o -c bpf_cubic.c
 
 # loading the created bpf_cubic.o object file into kernel using bpftool
+sudo bpftool struct_ops register bpf_cubic.o
